bcbbcc1593ab1d31df7498cd802d03c2f49f8318
reem282 <reem.al.dosari@outlook.com>
2023-02-16 20:50:19 +0300

    update

---
+++ code/Photo.java
@@ -0,0 +1,8 @@
+public class Photo {
+	// Constructor
+	public Photo(String path, LinkedList<String> tags);
+	// Return the path (full file name) of the photo. A photo is uniquely identified by its path.
+	public String getPath();
+	// Return all tags associated with the photo
+	public LinkedList<String> getTags();
+}
